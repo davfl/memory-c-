256af8095eb1247103f86f133dbad398dac211dc
davide florini <florinidavide@istitutocorni.it>
2024-03-01 21:01:52 +0100

    push gioco completo

---
+++ gioco/gioco/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace gioco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAvvia = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblMosse = new System.Windows.Forms.Label();
+            this.lblTitolo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAvvia
+            // 
+            this.btnAvvia.BackColor = System.Drawing.Color.Transparent;
+            this.btnAvvia.Location = new System.Drawing.Point(152, 277);
+            this.btnAvvia.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAvvia.Name = "btnAvvia";
+            this.btnAvvia.Size = new System.Drawing.Size(217, 74);
+            this.btnAvvia.TabIndex = 2;
+            this.btnAvvia.Text = "START";
+            this.btnAvvia.UseVisualStyleBackColor = false;
+            this.btnAvvia.Click += new System.EventHandler(this.btnAvvia_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(422, 444);
+            this.lblScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(76, 17);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "SCORE:  0";
+            this.lblScore.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblMosse
+            // 
+            this.lblMosse.AutoSize = true;
+            this.lblMosse.Location = new System.Drawing.Point(650, 444);
+            this.lblMosse.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMosse.Name = "lblMosse";
+            this.lblMosse.Size = new System.Drawing.Size(167, 17);
+            this.lblMosse.TabIndex = 6;
+            this.lblMosse.Text = "MOSSE RIMANENTI: 25  ";
+            this.lblMosse.Visible = false;
+            this.lblMosse.Click += new System.EventHandler(this.lblMosse_Click);
+            // 
+            // lblTitolo
+            // 
+            this.lblTitolo.Font = new System.Drawing.Font("Lucida Handwriting", 91F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitolo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblTitolo.Location = new System.Drawing.Point(-27, 28);
+            this.lblTitolo.Name = "lblTitolo";
+            this.lblTitolo.Size = new System.Drawing.Size(946, 245);
+            this.lblTitolo.TabIndex = 7;
+            this.lblTitolo.Text = "Memory";
+            this.lblTitolo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1435, 562);
+            this.Controls.Add(this.lblTitolo);
+            this.Controls.Add(this.lblMosse);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnAvvia);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAvvia;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblMosse;
+        private System.Windows.Forms.Label lblTitolo;
+    }
+}
+
---
+++ gioco/gioco/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gioco
+{
+    public partial class Form1 : Form
+    {
+        const int NumeroCarte = 20; //numero di carte
+        PictureBox [] carte; //array delle carte
+        PictureBox[] carte_retro; //array che contienre il retro delle carte (e serve per stamparle a scehrmo e sono cliccabili)
+        Random rand; 
+        int cont; //contatore che serve per il controllo (serve per vedere quante carte sono state scoperte (una o due))
+        int punteggio;
+        int cartaScoperta1;
+        int cartaScoperta2;
+        int[] numRandom;
+        int mosse;
+        int secret;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            carte = new PictureBox[NumeroCarte];
+            carte_retro = new PictureBox[NumeroCarte];
+            rand = new Random();
+            cont = 0;
+            punteggio = 0;
+            cartaScoperta1=0;
+            cartaScoperta2=0;
+            numRandom = new int[NumeroCarte];
+            mosse=25;
+            secret = 0;
+        }
+
+        private void btnAvvia_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < NumeroCarte; i++)
+            {
+                carte_retro[i] = new PictureBox();
+                carte_retro[i].Parent = this;
+                if (i < 10)
+                {
+                    carte_retro[i].Left = 25 + (i * 100);
+                    carte_retro[i].Top = 45;
+                }
+                else
+                {
+                    carte_retro[i].Left = 25 + ((i - 10) * 100);
+                    carte_retro[i].Top = 200;
+                }
+                carte_retro[i].Load("retro.jpg");
+                carte_retro[i].Width = 65;
+                carte_retro[i].Height = 100;
+                carte_retro[i].SizeMode = PictureBoxSizeMode.StretchImage;           
+                carte_retro[i].Click += new EventHandler(pictureCarte_Click);
+
+            }
+
+            caricaCarte();
+            posizionamentoCarte();
+            btnAvvia.Visible = false;
+            lblTitolo.Visible = false;
+            lblScore.Visible = true;
+            lblMosse.Visible = true;
+
+        }
+        private void posizionamentoCarte()
+        {
+            
+            int numero;
+
+            for (int i=0; i<NumeroCarte; i++)
+            {
+                do
+                {
+                    numero = rand.Next(0, 20);
+
+                } while (carte[numero].Visible == true);
+
+                carte[numero].Location = carte_retro[i].Location;
+                carte[numero].Visible = true;
+                numRandom[i] = numero;
+            }
+        }
+        private void pictureCarte_Click(object sender, EventArgs e)
+        {
+
+            PictureBox p = (PictureBox)sender;
+
+            for (int i=0; i < 20; i++)
+            {
+                if (carte_retro[i].Location == p.Location)
+                {
+                    carte_retro[i].Visible = false;
+                    cont++;
+
+                    if (cont == 1)
+                    {
+                        cartaScoperta1 = i;
+                    }
+                    else if (cont==2)
+                    {
+                        cartaScoperta2 = i;
+                    }
+                }
+            }
+
+            if (cont == 2)
+            {
+                if (numRandom[cartaScoperta1]+10==numRandom[cartaScoperta2] || numRandom[cartaScoperta1]==numRandom[cartaScoperta2]+10){
+                    punteggio += 2;
+                    mosse--;
+                    lblScore.Text = "SCORE: " + punteggio;
+                    cont = 0;
+                    
+                }
+                else
+                {
+                    timer1.Start();
+                    mosse--;
+                    cont = 0;
+                }
+                lblMosse.Text = "MOSSE RIMANENTI: " + mosse;
+                controlloWin(mosse, punteggio);
+            }
+        }
+        private void controlloWin(int mosse,int punteggio)
+        {
+            if (punteggio == 20)
+            {
+                MessageBox.Show("Congratulation you won!!");
+                Close();
+            }
+            if (mosse == 0)
+            {
+                MessageBox.Show("Retry again you will be more lucky next time");
+                Close();
+            }
+        }
+        private void caricaCarte()
+        {
+            for (int i = 0; i < NumeroCarte; i++)
+            {
+                carte[i] = new PictureBox();
+                carte[i].Parent = this;
+                if (i<10)
+                {
+                    carte[i].Load(i + ".jpg");
+                }
+                else
+                {
+                    carte[i].Load((i-10) + ".jpg");
+                }
+                carte[i].Width = 65;
+                carte[i].Height = 100;
+                carte[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                carte[i].Visible = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            carte_retro[cartaScoperta1].Visible = true;
+            carte_retro[cartaScoperta2].Visible = true;
+
+            timer1.Stop();
+        }
+
+        private void lblMosse_Click(object sender, EventArgs e)
+        {
+            secret++;
+            if (secret == 1)
+            {
+                lblMosse.Text = "Stop.";
+            }
+            if (secret == 2)
+            {
+                lblMosse.Text = "Why you did it again";
+            }
+            if (secret == 3)
+            {
+                lblMosse.Text = "...";
+            }
+            if (secret == 4)
+            {
+                lblMosse.Text = "Do you think you will get something special?";
+            }
+            if (secret == 5)
+            {
+                lblMosse.Text = "No";
+            }
+            if (secret == 6)
+            {
+                lblMosse.Text = "You will not give up untile a prize right?";
+            }
+            if (secret == 7)
+            {
+                lblMosse.Text = "Ok then here your prize";
+                MessageBox.Show("Congratulation you found out the secret ending");
+                Close();
+            }
+        }
+    }
+}
